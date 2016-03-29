cb54357915e0b4b65f4ea29168a1922edd131cb0
Alex Günsche <mail@lxg.de>
2016-03-29 12:52:05 +0200

    reflecting new namespace of agit.intl

--- Resources/public/js/ctl/page.unauthorized.js
+++ Resources/public/js/ctl/page.unauthorized.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
 
     var $page = new agit.context.Page(
-        agit.intl.L10n.t("Login"), {
+        agit.intl.t("Login"), {
             form : new agit.elem.ApiForm(
                 "profile.v1/Session.login",
                 [
-                    new agit.elem.FormRow("email", agit.intl.L10n.t("E-mail"), new agit.field.Text()),
-                    new agit.elem.FormRow("password", agit.intl.L10n.t("Password"), new agit.field.Password()),
+                    new agit.elem.FormRow("email", agit.intl.t("E-mail"), new agit.field.Text()),
+                    new agit.elem.FormRow("password", agit.intl.t("Password"), new agit.field.Password()),
                 ],
                 new agit.elem.FormFooter(),
                 function(res) { res.success && location.reload(); }
