d85d2425165fb5d31c2a2f7924f56e968da238b9
Alexander Günsche <mail@lxg.de>
2016-05-25 13:44:49 +0200

    migrate JS namespaces

--- Resources/public/js/ctl/page.unauthorized.js
+++ Resources/public/js/ctl/page.unauthorized.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
 
     var $page = new ag.ui.ctxt.Page(
-        agit.intl.t("Login"), {
+        ag.intl.t("Login"), {
             form : new ag.ui.elem.ApiForm(
                 "profile.v1/Session.login",
                 [
-                    new ag.ui.elem.FormRow("email", agit.intl.t("E-mail"), new ag.ui.field.Text()),
-                    new ag.ui.elem.FormRow("password", agit.intl.t("Password"), new ag.ui.field.Password()),
+                    new ag.ui.elem.FormRow("email", ag.intl.t("E-mail"), new ag.ui.field.Text()),
+                    new ag.ui.elem.FormRow("password", ag.intl.t("Password"), new ag.ui.field.Password()),
                 ],
                 new ag.ui.elem.FormFooter(),
                 function(res, status) { status === 200 && location.reload(); }
